989fa30d573bf49d410382a61c606420c971fb7e
Giordano Fechio <giordano.fechio@booking.com>
2020-06-19 17:34:14 +0200

    Refact: Terraform:

--- terraform/k8s/alb-ingress-controller.tf
+++ terraform/k8s/alb-ingress-controller.tf
@@ -26,7 +26,7 @@ resource "kubernetes_deployment" "alb_ingress_controller" {
         container {
           name  = "alb-ingress-controller"
           image = "docker.io/amazon/aws-alb-ingress-controller:v1.1.4"
-          args  = ["--ingress-class=alb", "--cluster-name=eks", "--aws-vpc-id=NEW-VPC-ID", "--aws-region=REGION"]
+          args  = ["--ingress-class=alb", "--cluster-name=eks", "--aws-vpc-id=var.vpc_id", "--aws-region=var.aws_region"]
         }
 
         service_account_name = "alb-ingress-controller"
---
+++ terraform/k8s/variables.tf
@@ -0,0 +1,4 @@
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
+variable "vpc_id" {}
+
